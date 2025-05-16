ac6307e959183262124e2be0f9dc2090845babc5
VandanaVB <vandana.bv@axeno.co>
2025-05-16 16:33:09 +0530

    added product slider and tabs

--- blocks/product-slider/product-slider.js
+++ blocks/product-slider/product-slider.js
@@ -17,4 +17,12 @@ export default function decorate(block) {
       panels[index]?.classList.add('active');
     });
   });
+
+    const images = block.querySelectorAll('img');
+    images.forEach((img) => {
+      const wrapper = document.createElement('div');
+      wrapper.className = 'product-image';
+      img.parentNode.insertBefore(wrapper, img);
+      wrapper.appendChild(img);
+    });
 }
