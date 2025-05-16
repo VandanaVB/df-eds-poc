76e7c45ce9526bdcf1a38a5bf799a8495d8eeb3c
VandanaVB <vandana.bv@axeno.co>
2025-05-16 16:56:12 +0530

    added product slider and tabs

--- blocks/product-tab/product-tab.js
+++
@@ -1,9 +0,0 @@
-export default function decorate(block) {
-    const images = block.querySelectorAll('img');
-    images.forEach((img) => {
-      const wrapper = document.createElement('div');
-      wrapper.className = 'product-image';
-      img.parentNode.insertBefore(wrapper, img);
-      wrapper.appendChild(img);
-    });
-}
